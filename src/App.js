5801dc4a789accd76452a177b3c0eae095c9d5c6
И Марк <imark.work.ac@gmail.com>
2021-09-30 21:21:57 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
 
 function App() {
+  const ref = useRef(null);
+
+  const handleFocus = () => {
+    console.log(ref.current);
+    ref.current.focus();
+    ref.current.style.color = "red";
+  };
   return (
     <div className="App">
-
+      <div>
+        <h1>FORM</h1>
+        <input ref={ref} />
+        <button onClick={handleFocus}>Focus</button>
+      </div>
     </div>
   );
 }
