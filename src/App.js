8b42d3a2386031f5ad7d67375ab140446b2402a9
И Марк <imark.work.ac@gmail.com>
2021-10-01 19:11:25 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [numbers, setNumbers] = useState([1, 2, 3]);
+
+  const addNumber = () => {
+    setNumbers([...numbers, numbers.length + 1]);
+  };
   return (
     <div className="App">
-
+      <ul>
+        {numbers.map((number, index) => (
+          <li key={index}>{number}</li>
+        ))}
+      </ul>
+      <button onClick={addNumber}>Add Number</button>
     </div>
   );
 }
