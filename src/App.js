2f422ce58767a1be29dbec0612f14a6ec974522b
И Марк <imark.work.ac@gmail.com>
2021-10-01 19:46:19 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [numbers, setNumbers] = useState([1, 2, 3]);
+  const [numbers, setNumbers] = React.useState([1, 2, 3, 4, 5, 6, 7]);
+  const timerRef = React.useRef();
 
   const addNumber = () => {
-    setNumbers([...numbers, numbers.length + 1]);
+    setNumbers((prev) => [...prev, prev.length + 1]);
+  };
+
+  const handleScroll = () => console.log("scroll");
+
+  // React.useEffect(() => {
+  //   ulRef.current.addEventListener("scroll", handleScroll);
+  // }, []);
+
+  // const removeScroll = () => {
+  //   ulRef.current.removeEventListener("scroll", handleScroll);
+  // };
+
+  const start = () => {
+    timerRef.current = setInterval(addNumber, 1000);
+  };
+
+  const stop = () => {
+    clearInterval(timerRef.current);
   };
   return (
     <div className="App">
+      <button onClick={addNumber}>Add Number</button>
+      <button onClick={start}>start</button>
+      <button onClick={stop}>stop</button>
       <ul>
         {numbers.map((number, index) => (
           <li key={index}>{number}</li>
         ))}
       </ul>
-      <button onClick={addNumber}>Add Number</button>
     </div>
   );
 }
