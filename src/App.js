5633e42a56fb8b37701c3e163bb65c1f64481fd1
И Марк <imark.work.ac@gmail.com>
2021-10-03 12:22:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./App.css";
-import { Input } from "./Input";
 
 function App() {
-  const firstNameRef = useRef(null);
-  const lastNameRef = useRef(null);
-  const submitRef = useRef(null);
-
-  useEffect(() => {
-    firstNameRef.current.focus();
-  });
-
-  const firstKeyDown = (e) => {
-    if (e.key === "Enter") {
-      lastNameRef.current.focus();
-    }
-  };
-  const lastKeyDown = (e) => {
-    if (e.key === "Enter") {
-      submitRef.current.focus();
-    }
-  };
-  const submitKeyDown = (e) => {
-    alert("Form submitted");
-  };
   return (
     <div className="App">
-      <Input
-        placeholder="first name"
-        ref={firstNameRef}
-        onKeyDown={firstKeyDown}
-      />
-      <Input
-        placeholder="last name"
-        ref={lastNameRef}
-        onKeyDown={lastKeyDown}
-      />
-      <Input
-        type="submit"
-        value="Submit"
-        ref={submitRef}
-        onKeyDown={submitKeyDown}
-      />
     </div>
   );
 }
