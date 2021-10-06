3e26944df496cee8a32e35f76526ab55b8d59c4b
И Марк <imark.work.ac@gmail.com>
2021-10-06 22:38:14 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import TopicsScreen from "./containers/TopicsScreen";
+import { setCount } from "./reposReducer";
 
 function App() {
+  const inputRef = useRef(null);
+
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  const handleCount = () => {
+    dispatch(setCount(inputRef.current.valueAsNumber));
+  };
+
   return (
-      <div className="App">
-        <TopicsScreen />
-      </div>
+    <div className="App">
+      <input ref={inputRef} type="number" defaultValue="0" />
+      <button onClick={() => handleCount()}>COUNT</button>
+      <h3>{count}</h3>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from "./App";
+import reposReducer from "./reposReducer";
 
-import * as reducers from "./store/reducers";
-const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+const store = createStore(reposReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
